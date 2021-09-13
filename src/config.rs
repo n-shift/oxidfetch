04f8a92492a7a09e67df04ec30fd8c7203d8abc9
shift-d <me@shift-d.tk>
2021-09-13 17:06:57 +0300

    feat(config): proper error handling

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
-// TODO(thiserror): proper handling
-use std::convert::From;
+use anyhow::{Context, Result};
+use std::convert::{TryFrom, TryInto};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Config {
@@ -23,15 +23,17 @@ struct Component {
 
 type MsgPack = Vec<u8>;
 
-impl From<MsgPack> for Config {
-    fn from(buf: MsgPack) -> Self {
-        rmp_serde::from_read_ref::<&[u8], Self>(&&buf[..]).unwrap()
+impl TryFrom<MsgPack> for Config {
+    type Error = anyhow::Error;
+    fn try_from(buf: MsgPack) -> Result<Self> {
+        rmp_serde::from_read_ref::<&[u8], Self>(&&buf[..]).context("Failed to create config from msgpack")
     }
 }
 
-impl From<Config> for MsgPack {
-    fn from(config: Config) -> Self {
-        rmp_serde::to_vec(&config).unwrap()
+impl TryFrom<Config> for MsgPack {
+    type Error = anyhow::Error;
+    fn try_from(config: Config) -> Result<Self> {
+        rmp_serde::to_vec(&config).context("Failed to create msgpack structure from config")
     }
 }
 
@@ -39,7 +41,7 @@ impl From<Config> for MsgPack {
 mod tests {
     use super::*;
     #[test]
-    fn msgpack_to_config() {
+    fn msgpack_to_config() -> Result<()> {
         let comp = Component {
             name: "OS".into(),
             icon: Some("!".into()),
@@ -55,13 +57,15 @@ mod tests {
             0x92, 0x81, 0x2, 0xc0, 0x91, 0x93, 0xa2, 0x4f, 0x53, 0xa1, 0x21, 0xa7, 0x53,
             0x6f, 0x6d, 0x65, 0x20, 0x4f, 0x53,
         ];
-        let cfg: Config = buf.into();
+        let cfg: Config = buf.try_into().unwrap();
 
         assert_eq!(cfg, expected_cfg);
+
+        Ok(())
     }
 
     #[test]
-    fn msgpack_from_config() {
+    fn msgpack_from_config() -> Result<()> {
         let comp = Component {
             name: "OS".into(),
             icon: Some("!".into()),
@@ -73,12 +77,14 @@ mod tests {
             components: vec![comp],
         };
 
-        let buf: MsgPack = cfg.into();
+        let buf: MsgPack = cfg.try_into().unwrap();
         let expected_buf: MsgPack = vec![
             0x92, 0x81, 0x2, 0xc0, 0x91, 0x93, 0xa2, 0x4f, 0x53, 0xa1, 0x21, 0xa7, 0x53,
             0x6f, 0x6d, 0x65, 0x20, 0x4f, 0x53,
         ];
 
         assert_eq!(buf, expected_buf);
+
+        Ok(())
     }
 }
