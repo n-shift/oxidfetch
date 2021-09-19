8eb1766ac607b72654bb0dbd60c5b26975367b7f
shift-d <me@shift-d.tk>
2021-09-19 21:51:11 +0300

    chore(main): basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 #[macro_use]
 extern crate serde_derive;
 
+use anyhow::Result;
+
 mod config;
 mod render;
 mod script;
 
-fn main() {
-    todo!();
+fn main() -> Result<()> {
+    let cfg = script::extract_config()?;
+    render::display(cfg);
+
+    Ok(())
 }
