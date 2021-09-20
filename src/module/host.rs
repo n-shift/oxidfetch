1d7f6ee3a571ba9558ca475ea1309966049975d3
shift-d <me@shift-d.tk>
2021-09-20 12:12:28 +0300

    refactor(module): use whoami crate for fetching host

--- src/module/host.rs
+++ src/module/host.rs
@@ -1,10 +1,3 @@
-use gethostname::gethostname;
-use std::env;
-
 pub fn fetch() -> Vec<String> {
-    #[cfg(target_os = "windows")]
-    let username = env::var("USERNAME").unwrap();
-    #[cfg(not(target_os = "windows"))]
-    let username = env::var("USER").unwrap();
-    vec![username, gethostname().into_string().unwrap()]
+    vec![whoami::username(), whoami::hostname()]
 }
