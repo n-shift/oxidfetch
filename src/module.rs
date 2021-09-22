dacc813939a29e359c2abb3b210ffcc2c1e44c88
shift-d <me@shift-d.tk>
2021-09-22 16:03:41 +0300

    refactor(module): proper displaying of units

--- src/module.rs
+++ src/module.rs
@@ -12,42 +12,78 @@ pub mod os {
 
 pub mod uptime {
     use super::convert_seconds;
-    use sysinfo::{System, SystemExt, RefreshKind};
+    use sysinfo::{RefreshKind, System, SystemExt};
 
     pub fn fetch() -> String {
         let sys = System::new_with_specifics(RefreshKind::new());
-        format!("{}", convert_seconds(sys.uptime() as f64))
+        convert_seconds(sys.uptime() as f64)
     }
 }
 
 pub mod memory {
     use super::convert_kilobytes;
-    use sysinfo::{System, SystemExt, RefreshKind};
+    use sysinfo::{RefreshKind, System, SystemExt};
 
     pub fn fetch() -> String {
         let sys = System::new_with_specifics(RefreshKind::new().with_memory());
-        format!("{}/{}", convert_kilobytes(sys.used_memory() as f64), convert_kilobytes(sys.total_memory() as f64))
+        format!(
+            "{}/{}",
+            convert_kilobytes(sys.used_memory() as f64),
+            convert_kilobytes(sys.total_memory() as f64)
+        )
     }
 }
 
 use std::cmp;
 
 pub fn convert_kilobytes(num: f64) -> String {
-  let units = ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-  if num < 1_f64 {
-      return format!("{} {}", num, "kB");
-  }
-  let delimiter = 1000_f64;
-  let exponent = cmp::min((num.ln() / delimiter.ln()).floor() as i32, (units.len() - 1) as i32);
-  let pretty_bytes = format!("{:.2}", num / delimiter.powi(exponent)).parse::<f64>().unwrap() * 1_f64;
-  let unit = units[exponent as usize];
-  format!("{} {}", pretty_bytes, unit)
+    let units = ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    if num < 1_f64 {
+        return format!("{} {}", num, "kB");
+    }
+    let delimiter = 1000_f64;
+    let exponent = cmp::min(
+        (num.ln() / delimiter.ln()).floor() as i32,
+        (units.len() - 1) as i32,
+    );
+    let pretty_bytes = format!("{:.2}", num / delimiter.powi(exponent))
+        .parse::<f64>()
+        .unwrap()
+        * 1_f64;
+    let unit = units[exponent as usize];
+    format!("{} {}", pretty_bytes, unit)
 }
-// https://stackoverflow.com/a/8273826
-pub fn convert_seconds(num: f64) -> String {
-    let days = (num / (60 * 60 * 24) as f64).floor();
-    let hours = ((num % (60 * 60 * 24) as f64) / (60 * 60) as f64).floor();
-    let minutes = (((num % (60 * 60 * 24) as f64) % (60 * 60) as f64) / (60 * 60) as f64).floor();
 
-    format!("{}d {}h {}m", days, hours, minutes)
+pub fn convert_seconds(input: f64) -> String {
+    const MIN: f64 = 60.;
+    const HOUR: f64 = MIN * 60.;
+    const DAY: f64 = HOUR * 24.;
+    let days = (input / DAY).floor();
+    let hours = ((input % DAY) / HOUR).floor();
+    let mins = (((input % DAY) % HOUR) / MIN).floor();
+
+    format!(
+        "{}{}{}",
+        {
+            if days == 0. {
+                String::new()
+            } else {
+                format!("{}d ", days)
+            }
+        },
+        {
+            if hours == 0. {
+                String::new()
+            } else {
+                format!("{}h ", hours)
+            }
+        },
+        {
+            if mins == 0. {
+                String::new()
+            } else {
+                format!("{}m ", mins)
+            }
+        }
+    )
 }
